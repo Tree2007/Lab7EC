e30ede9170b9ab372164c4a2f55d6f0f9653cf3a
Forest <fhaughton6@gmail.com>
2024-10-17 08:47:34 -0400

    Initial commit

---
+++ src/Initial.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 2; i++) {
+            for (int j = 0; j < 11; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= 2; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
